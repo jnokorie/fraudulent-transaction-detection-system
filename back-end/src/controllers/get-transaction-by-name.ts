1c4ebde948097bef2bc2e11144086e552246c3eb
Jnkemokorie <160935632+Jnkemokorie@users.noreply.github.com>
2026-03-09 00:53:46 +0000

    some more refactoring

--- back-end/src/controllers/get-transaction-by-name.ts
+++ back-end/src/controllers/get-transaction-by-name.ts
@@ -3,23 +3,25 @@ import { Request, Response } from 'express'
 import Transaction from '../types/transaction-types'
 
 
-const getTransactionsByName = (req: Request, res: Response) => {
-    db.all("SELECT * FROM Transactions WHERE user_name = $user_name", { $user_name: req.params.user_name},
-        (error, transactions: Transaction[]) => {
-            if (error) {
-                res.status(500).json({error: error.message})
-            }
-            if (transactions.length) {
-                res.status(200).json(transactions[0])
-            }
-            else{
-                res.status(404).json({
-                    error: "Transactions not found"
-                })
 
-            }
+const getTransactionsByName = (req: Request, res: Response) => {
+    try {
+        const allTransactions: Transaction[] = db.prepare("SELECT * FROM Transactions WHERE user_name = @user_name").all({ user_name: req.params.user_name }) as Transaction[]
+        if (allTransactions.length) {
+            res.status(200).json(allTransactions[0])
         }
-    )
+        else {
+            res.status(404).json({
+                error: "Transactions not found"
+            })
+        }
+    }
+    catch (error) {
+        if (error instanceof Error) {
+            res.status(500).json({ error: error.message })
+        }
+    }
+
 }
 
 export default getTransactionsByName
