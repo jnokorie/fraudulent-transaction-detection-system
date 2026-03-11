86963641efed31bfb94e5a0e2903b193d88daaf2
Jnkemokorie <160935632+Jnkemokorie@users.noreply.github.com>
2026-03-11 02:24:27 +0000

    completed backend

--- back-end/src/controllers/create-transaction.ts
+++ back-end/src/controllers/create-transaction.ts
@@ -1,28 +1,30 @@
 import db from '../database/database-schema';
 import { Request, Response } from 'express'
-import {Transaction, CurrUser} from '../types/transaction-types'
+import { Transaction } from '../types/transaction-types'
+import transactionLimit from '../services/business-logic/high-value-anomoly'
+import transactionSpeed from '../services/business-logic/time-period'
 
 const createTransaction = (req: Request, res: Response) => {
     const date: Date = new Date()
-    const recentUser = db.prepare("SELECT ")
 
-    
-    
+    try {
+        const recentUser: Transaction[] = db.prepare("SELECT * FROM Transactions ORDER BY id DESC LIMIT 1").get() as Transaction[]
+        const isFlagged: boolean = (transactionSpeed(req.body, recentUser, date) || transactionLimit(req.body.amount))
+        db.prepare("INSERT INTO Transactions(user_name, amount, time, flagged) VALUES(@user_name, @amount, @time, @flagged)")
+            .get({
+                user_name: req.body.user_name,
+                amount: req.body.amount,
+                time: date.toString(),
+                flagged: isFlagged
+            })
+        res.status(200).json({message: "Successful Input!"})
+    }
+
+    catch (error) {
+        if (error instanceof Error) {
+            res.status(500).json({ error: error.message })
+        }
+    }
 }
-
-
 export default createTransaction
 
-
-db.get(`INSERT INTO Transactions(user_name, amount, time, flagged) 
-          VALUES($user_name, $amount, $time, $flagged)`, 
-        {
-            $user_name: req.body.user_name,
-            $amount: req.body.amount,
-            $time: req,
-            $flagged:req
-        }, (error) => {
-            if (error){
-                res.status(500).json({error: error.message})
-            }
-        })
